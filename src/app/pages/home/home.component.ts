7eae092d073fa62cfbabfb9071dcc1a88b73036a
Rafael Ananias <rafaelananias33@gmail.com>
2025-02-19 14:02:52 -0300

    bootstrap

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsComponent } from "../../_components/forms/forms.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsComponent],
   templateUrl: './home.component.html',
   //styleUrl: './home.component.css'
   styleUrl: '/ ../../node_modules/bootstrap/dist/css/bootstrap.min.css'
