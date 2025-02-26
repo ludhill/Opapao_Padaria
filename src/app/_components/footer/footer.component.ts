e8d7057ace87d9cc9740eebc310bba3b9102b7d6
ludhill <LucasDenner.hs@live.com>
2025-02-26 07:34:32 -0300

    Corrigindo Footer odeio trabalho em grupo

--- src/app/_components/footer/footer.component.ts
+++ src/app/_components/footer/footer.component.ts
@@ -1,10 +1,11 @@
+import { Component,input,inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './footer.component.html',
   //styleUrl: './footer.component.css'
   styleUrl: '/ ../../node_modules/bootstrap/dist/css/bootstrap.min.css'
